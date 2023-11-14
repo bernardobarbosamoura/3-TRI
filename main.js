f560ba9b9df53e8136a7c906e5e5453bf1341194
bernardobarbosamoura <140512085+bernardobarbosamoura@users.noreply.github.com>
2023-11-14 08:34:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 function tocaSom (idElementoAudio){
-      const elemento = document.querySelector(idElementoAudio).play();   
+      const elemento = document.querySelector(idElementoAudio);   
       if(elemento === null) {
         alert('Elemento não encontrado');
       }
-}
+      if (elemento != null && elemento.localName === 'audio'){
+          elemento.play();
+      }else {
+         alert('Elemento não encontrado');
+
+}}
  const listaDeTeclas = document.querySelectorAll('.tecla');
  //Estrutura de repetição - Enquanto 
  for(let contador =0;contador < listaDeTeclas.length;contador++){
     const tecla =listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = '#som_ ${instrumento}';
+    const idAudio = `#som_${instrumento}`;
 
     console.log(idAudio);
 
     tecla.onclick = function(){
        tocaSom(idAudio);
     };
-    tecla.onkeydown = function () {
+    tecla.onkeydown = function(evento) {
        if (evento.code ==='Enter'  || evento.code === 'Space'){
        tecla.classList.add('ativa');
     }
 };
-    tecla.onkeyup = function () {
+    tecla.onkeyup = function() {
        tecla.classList.remove('ativa');
     };     
     console.log(instrumento);
